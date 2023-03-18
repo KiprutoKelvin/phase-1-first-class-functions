ba8a3e795562530db330141b8685527839b53f23
Kiru <kelvin.kipruto@student.moringaschool.com>
2023-03-18 18:27:39 +0300

    Completed assgnment

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  function returnsANamedFunction() {
+    function myNamedFunction() {
+        console.log("This is a named function");
+      }
+    return myNamedFunction;
+  }
+  function returnsAnAnonymousFunction(){
+    return function(){
+        console.log("Anonymous");
+    }
+  }
+  
